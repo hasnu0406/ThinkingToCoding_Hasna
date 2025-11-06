7e303e1505d1130596fcefcca8801e2ee250e6c0
HASNA MUBARAK AZEEM <135305537+hasnu0406@users.noreply.github.com>
2025-11-06 20:31:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+sequence=['Boil the water', 
+'Add tea powder and sugar',
+'Add milk',
+'Boil for a while', 
+'Turn of the flame', 
+'Strain off and serve']
+for steps in range(0,len(sequence)):
+    print(sequence[steps],"\n")
+print("Tea is ready!!")
