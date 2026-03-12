cf79c0dc54b9fdedec3c33ccd76b72fea1bb6300
HASNA MUBARAK AZEEM <hasna040306@gmail.com>
2026-03-12 15:57:21 +0530

    Added config file for environment variables

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+DB_NAME = os.getenv("DB_NAME")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+
+API_KEY = os.getenv("API_KEY")
